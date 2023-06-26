c7b8978cc73268579c13fc37754a8a4e51a3725f
Jose <nietojose1995@gmail.com>
2023-06-26 23:19:03 +0100

    adding image gallery

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,7 @@
-import { Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 //import { MailOutline } from '@mui/icons-material';
 
 export const JournalPage = () => {
@@ -9,8 +10,22 @@ export const JournalPage = () => {
     <JournalLayout>
       
       {/* <Typography>asdfasdfdsafdasfadssdfdsafdsfasdfasf </Typography> */}
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size='large'
+        sx={{
+          color:'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9},
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }}/>
+      </IconButton>
     </JournalLayout>
 
     )
